e525013752b791b2f5ccbf1fc94d026d82f6f524
windwave51 <makamia3@gmail.com>
2026-05-25 14:37:59 +0000

    feat: 한국시간(KST) 적용

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from datetime import datetime, timedelta
 import pytz
 import pytz
 import pytz
+import pytz
 
 st.set_page_config(page_title='Trade Signal', page_icon='📊', layout='wide')
 
@@ -48,6 +49,7 @@ st.markdown('''<style>
 APP_KEY    = st.secrets['APP_KEY']
 APP_SECRET = st.secrets['APP_SECRET']
 BASE_URL   = 'https://openapi.koreainvestment.com:9443'
+KST        = pytz.timezone('Asia/Seoul')
 KST        = pytz.timezone('Asia/Seoul')
 KST        = pytz.timezone('Asia/Seoul')
 KST        = pytz.timezone('Asia/Seoul')
